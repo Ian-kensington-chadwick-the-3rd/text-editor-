0cc961f87087ea85b6595d8ec75a5a26d42f8ec2
Ian Sills <iansills04@gmail.com>
2023-11-28 00:59:27 -0600

    image appears but my database is still doesnt work calling it a night im going to bed

---
+++ client/webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      header: './src/js/header.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Client Server',
+        template: './index.html',
+      }),
+      new GenerateSW(),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'text-editor',
+        short_name: 'te',
+        description: 'a text editor that is used in browser that can be downloaded as pwa',
+        backround_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+           src: path.resolve('./src/images/logo.png'),
+           sizes: [96, 128, 192, 256, 384, 512],
+           destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader','css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
